5c8553b5d14e5854cef121cd095cb86bafac3a55
Michael Sullivan <sullivanm20@berkeley.edu>
2017-10-24 02:22:15 -0700

    Rough skeleton

--- compile-classes.py
+++ compile-classes.py
@@ -1,5 +1,29 @@
-""" This script parses through the UC Berkeley majors page,
+""" Script to parse through the UC Berkeley majors page,
     and compiles a list of classes required to declare each
     major. """
+import sys
 
-def 
+URL = http://guide.berkeley.edu/undergraduate/degree-programs/
+
+def main():
+    """Main entry point for script."""
+    major_links = get_major_links(URL)
+    major_prereqs = dict()
+    for major in major_links:
+        url = major_links[major]
+        prereqs = get_major_prereqs(url)
+        major_prereqs[major] = prereqs
+    pass
+
+def get_major_links(url):
+    """Returns a dictionary mapping major names to
+    the URL to their courses page."""
+    pass
+
+def get_major_prereqs(url):
+    """Takes a single major's web page and returns a list
+    of the classes needed to declare the major."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main)
