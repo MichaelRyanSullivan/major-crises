85f5f63509334bef8aa2a7bb1a154aa903383a01
Michael Sullivan <sullivanm20@berkeley.edu>
2017-11-11 14:12:18 -0800

    finished get_major_links. Could be cleaner

--- compile-classes.py
+++ compile-classes.py
@@ -4,32 +4,41 @@
 import sys
 from bs4 import BeautifulSoup as bs
 import requests as req
+import re
 
 URL = "http://guide.berkeley.edu/undergraduate/degree-programs/"
 
-def main():
-    """Main entry point for script."""
-    major_links = get_major_links(URL)
-    major_prereqs = dict()
-    for major in major_links:
-        url = major_links[major]
-        prereqs = get_major_prereqs(url)
-        major_prereqs[major] = prereqs
-    pass
+# def main():
+#     """Main entry point for script."""
+#     major_links = get_major_links(URL)
+#     major_prereqs = dict()
+#     for major in major_links:
+#         url = major_links[major]
+#         prereqs = get_major_prereqs(url)
+#         major_prereqs[major] = prereqs
+#     pass
 
 def get_major_links(url):
     """Returns a dictionary mapping major names to
     the URL to their courses page."""
-    soup = bs(req.get(url))
-    soup.prettify
-    print(soup.prettify)
+    soup = bs(req.get(url).text)
+    script = soup.find_all('script')[9]
+    strScript = str(script)
+    p_name = re.compile('name:"(.*)"')
+    p_url = re.compile('url:"(.*)"')
+    names = p_name.findall(strScript)
+    urls = p_url.findall(strScript)
+    assert(len(names) == len(urls))
+    d = {}
+    for i in range(len(names)):
+        d[names[i]] = urls[i]
 
-    pass
+    return d
 
 def get_major_prereqs(url):
     """Takes a single major's web page and returns a list
     of the classes needed to declare the major."""
     pass
 
-if __name__ == '__main__':
-    sys.exit(main)
+# if __name__ == '__main__':
+#     sys.exit(main)
