8d96c5417dc8a630684a5b64b203ce28808df696
Michael Sullivan <sullivanm20@berkeley.edu>
2017-12-20 12:28:51 -0500

    stabilized scraping for links

--- compile-classes.py
+++ compile-classes.py
@@ -8,21 +8,28 @@ import re
 
 URL = "http://guide.berkeley.edu/undergraduate/degree-programs/"
 
-# def main():
-#     """Main entry point for script."""
-#     major_links = get_major_links(URL)
-#     major_prereqs = dict()
-#     for major in major_links:
-#         url = major_links[major]
-#         prereqs = get_major_prereqs(url)
-#         major_prereqs[major] = prereqs
-#     pass
+def main():
+    """Main entry point for script."""
+    major_links = get_major_links(URL)
+    print(major_links)
+    # major_classes = dict()
+    # for major in major_links:
+    #     url = major_links[major]
+    #     major_classes[major] = get_major_classes(url)
+    pass
+
 def get_major_links(url):
     """Returns a dictionary mapping major names to
     the URL to their courses page."""
-    soup = bs(req.get(url).text)
-    script = soup.find_all('script')[9]
-    strScript = str(script)
+    soup = bs(req.get(url).text, "html.parser")
+    scripts = soup.find_all('script')
+    strScript = ''
+    print(scripts)
+    for script in scripts:
+        string = str(script)
+        if (len(string) > len(strScript)):
+            strScript = string
+    print(strScript)
     p_name = re.compile('name:"(.*)"')
     p_url = re.compile('url:"(.*)"')
     names = p_name.findall(strScript)
@@ -30,14 +37,14 @@ def get_major_links(url):
     assert(len(names) == len(urls))
     d = {}
     for i in range(len(names)):
-        d[names[i]] = URL + "/" + urls[i] + "/"
-
+        d[names[i]] = URL + urls[i] + "/"
     return d
 
-def get_major_prereqs(url):
+def get_major_classes(url):
     """Takes a single major's web page and returns a list
-    of the classes needed to declare the major."""
+    of the classes that count toward the major."""
+
     pass
 
-# if __name__ == '__main__':
-#     sys.exit(main)
+if __name__ == '__main__':
+    sys.exit(main)
