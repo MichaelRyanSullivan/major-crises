aed46c224ed85bca367dd47b319748ec41b37b9f
Michael Sullivan <sullivanm20@berkeley.edu>
2017-12-23 16:13:28 -0500

    debugged, wrote results of major/requirements mapping to file major_requirements.txt

--- compile-classes.py
+++ compile-classes.py
@@ -1,21 +1,26 @@
 """ Script to parse through the UC Berkeley majors page,
     and compiles a list of classes required to declare each
-    major. """
+    major. Writes a file to the repository of the format:
+    MAJOR1 CLASS1 CLASS2...
+    MAJOR2 CLASS1 ....
+    ...
+    """
 import sys
 from bs4 import BeautifulSoup as bs
 import requests as req
 import re
 
 URL = "http://guide.berkeley.edu/undergraduate/degree-programs/"
-
 def main():
     """Main entry point for script."""
     major_links = get_major_links(URL)
     print(major_links)
-    # major_classes = dict()
-    # for major in major_links:
-    #     url = major_links[major]
-    #     major_classes[major] = get_major_classes(url)
+    #Maps majors to a list of classes that fulfill requirements.
+    major_classes = dict()
+    for major in major_links:
+        url = major_links[major]
+        major_classes[major] = get_major_classes(url)
+    write_to_file(major_classes)
     pass
 
 def get_major_links(url):
@@ -24,12 +29,10 @@ def get_major_links(url):
     soup = bs(req.get(url).text, "html.parser")
     scripts = soup.find_all('script')
     strScript = ''
-    print(scripts)
     for script in scripts:
         string = str(script)
         if (len(string) > len(strScript)):
             strScript = string
-    print(strScript)
     p_name = re.compile('name:"(.*)"')
     p_url = re.compile('url:"(.*)"')
     names = p_name.findall(strScript)
@@ -43,8 +46,45 @@ def get_major_links(url):
 def get_major_classes(url):
     """Takes a single major's web page and returns a list
     of the classes that count toward the major."""
+    classes = []
+    soup = bs(req.get(url).text, "html.parser")
+    bubble_classes = soup.find_all("a", class_="bubblelink code")
+    # p_class = re.compile("showCourse\(this, '([A-Z ]+ *[A-Z]*[0-9]+[A-Z]*)'\)")
+    # p_class = re.compile("showCourse\(this, '(.*)'")
+    p_class = re.compile('href=".*">(.*)</a>')
+    p_num = re.compile(" *([A-Z]*[0-9]+[A-Z]*)")
+    p_name = re.compile("(([A-Z\/]+) )+")
+    prev = ""
+    for bubble in bubble_classes:
+        m = p_class.search(str(bubble))
+        _class = m.group(1)
+        print(repr(_class))
+        _class = _class.replace(u'\xa0', u' ')
+        _class = _class.replace(u'&amp;', u'&')
+        num_match = p_num.match(_class)
+        if num_match:
+            print(prev)
+            num = num_match.group().strip()
+            prev_name_match = p_name.match(prev)
+            prev_name = prev_name_match.group().strip()
+            _class = prev_name + " " + num
+        if _class not in classes:
+            classes.append(_class)
+            prev = _class
+    return classes
 
-    pass
+def write_to_file(major_classes):
+    """Takes a dictionary of majors to a list of classes that fulfill
+       requirements, MAJOR_CLASSES, and writes those requirements to
+       a file in the repository."""
+    with open('major_requirements.txt', 'wt+') as _file:
+        for major in major_classes:
+            _file.write(major + ";")
+            reqs = major_classes[major]
+            for req in reqs:
+                _file.write(" " + req)
+            _file.write("\n")
 
+main()
 if __name__ == '__main__':
     sys.exit(main)
